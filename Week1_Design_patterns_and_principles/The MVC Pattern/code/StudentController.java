44b0b201b7e734f14dc1808b17e7e3d7d6d81e0d
punyabrataguchhait <159674594+punyabrataguchhait@users.noreply.github.com>
2025-06-22 18:29:13 +0530

    Add files via upload

---
+++ Week1_Design_patterns_and_principles/The MVC Pattern/code/StudentController.java
@@ -0,0 +1,17 @@
+public class StudentController {
+    private Student model;
+    private StudentView view;
+
+    public StudentController(Student model, StudentView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void setStudentName(String name) {
+        model.setName(name);
+    }
+
+    public void updateView() {
+        view.displayStudentDetails(model);
+    }
+}
