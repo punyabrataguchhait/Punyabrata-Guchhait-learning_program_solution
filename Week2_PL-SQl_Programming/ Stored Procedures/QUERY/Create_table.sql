b6402137b591e40d8a700d77fc96b182d55fa6db
punyabrataguchhait <159674594+punyabrataguchhait@users.noreply.github.com>
2025-06-27 05:55:06 +0530

    Create Create_table.sql

---
+++ Week2_PL-SQl_Programming/ Stored Procedures/QUERY/Create_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Customers (
+    CustomerID NUMBER PRIMARY KEY,
+    Age NUMBER,
+    LoanInterestRate NUMBER(5,2),
+    Balance NUMBER,
+    IsVIP VARCHAR2(5),
+    LoanDueDate DATE
+);
