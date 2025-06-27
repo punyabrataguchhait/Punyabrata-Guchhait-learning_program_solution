a178b3e633ac4623ceae6d73ba6867dcfe79c4d5
punyabrataguchhait <159674594+punyabrataguchhait@users.noreply.github.com>
2025-06-27 06:12:31 +0530

    Delete Week2_PL-SQl_Programming/ Stored Procedures/QUERY directory

--- Week2_PL-SQl_Programming/ Stored Procedures/QUERY/Create_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Customers (
-    CustomerID NUMBER PRIMARY KEY,
-    Age NUMBER,
-    LoanInterestRate NUMBER(5,2),
-    Balance NUMBER,
-    IsVIP VARCHAR2(5),
-    LoanDueDate DATE
-);
