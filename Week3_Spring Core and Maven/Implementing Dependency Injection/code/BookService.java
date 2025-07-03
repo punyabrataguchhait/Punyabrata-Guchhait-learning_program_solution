4655b463710846dec6e7465bef48c8a9cbee0d9e
punyabrataguchhait <159674594+punyabrataguchhait@users.noreply.github.com>
2025-07-04 01:50:07 +0530

    Create BookService.java

---
+++ Week3_Spring Core and Maven/Implementing Dependency Injection/code/BookService.java
@@ -0,0 +1,18 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+    private BookRepository bookRepository;
+
+    public void addBook(String bookName) {
+        System.out.println("Adding book: " + bookName);
+        if (bookRepository != null) {
+            bookRepository.saveBook(bookName);
+        } else {
+            System.out.println("BookRepository not initialized!");
+        }
+    }
+
+   
+}
