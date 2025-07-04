50b7f877d1c8091e52a9e8a55bb9ab1a58379d2e
punyabrataguchhait <159674594+punyabrataguchhait@users.noreply.github.com>
2025-07-04 11:06:09 +0530

    Create QueryMethodsApplication.java

---
+++ Week 3_Spring Data JPA with Spring Boot Hibernate/Demonstrate implementation of Query Methods feature of Spring Data JPA/code/QueryMethodsApplication.java
@@ -0,0 +1,11 @@
+package com.example.querymethods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryMethodsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QueryMethodsApplication.class, args);
+    }
+}
