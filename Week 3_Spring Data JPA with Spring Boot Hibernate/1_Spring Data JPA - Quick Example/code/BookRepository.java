f3a292d32e59acff5f394f07d1f367c247437245
punyabrataguchhait <159674594+punyabrataguchhait@users.noreply.github.com>
2025-07-04 10:26:12 +0530

    Create BookRepository.java

---
+++ Week 3_Spring Data JPA with Spring Boot Hibernate/1_Spring Data JPA - Quick Example/code/BookRepository.java
@@ -0,0 +1,10 @@
+package com.example.libman.repository;
+
+import com.example.libman.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findByAuthor(String author);
+}
