08b21e9d74c7c193c0f9b912fccfd3a356233cec
punyabrataguchhait <159674594+punyabrataguchhait@users.noreply.github.com>
2025-07-04 10:21:56 +0530

    Create LibmanSpringJpaApplication.java

---
+++ Week 3_Spring Data JPA with Spring Boot Hibernate/1_Spring Data JPA - Quick Example/code/LibmanSpringJpaApplication.java
@@ -0,0 +1,11 @@
+package com.example.libman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibmanSpringJpaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LibmanSpringJpaApplication.class, args);
+    }
+}
