dcb1e0a955f5b5db61e50f5bb3ee7c047f8301c5
punyabrataguchhait <159674594+punyabrataguchhait@users.noreply.github.com>
2025-07-25 13:55:26 +0530

    Add files via upload

---
+++ Week6_React/2_StudentApp/code/src/Components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+  return (
+
+    <div style={{textAlign:'center'}}>
+            <h2>Welcome to the About page of the Student Management Portal</h2>
+    </div>
+    
+  );
+}
+
+export default About;
