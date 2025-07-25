94c26c2ce88e4c9a2393a5ba631e4908ebf2f79d
punyabrataguchhait <159674594+punyabrataguchhait@users.noreply.github.com>
2025-07-25 13:53:32 +0530

    Add files via upload

---
+++ Week6_React/1_myfirstreact/code/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App(){
+  return(
+    <div style ={{textAlign :'center'}}>
+      <h1>Welcome the first session of React</h1>
+    </div>
+    
+  );
+}
+
+export default App;
